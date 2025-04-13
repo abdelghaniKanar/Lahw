4c8a69d588972e59b531919a29db9e4fa48c9e2e
kanarAb <KanarAb.webapps001@gmail.com>
2025-04-13 22:47:59 +0100

    Project setup: Vite + TailwindCSS + React Router DOM + Folder structure initialized

---
+++ frontend/src/App.jsx
@@ -0,0 +1,24 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Detail from "./pages/Detail";
+import About from "./pages/About";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Navbar />
+      <div className="flex-grow">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/movie/:id" element={<Detail />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
